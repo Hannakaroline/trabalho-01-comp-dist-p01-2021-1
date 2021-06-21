1d6342250b1dfcf59b443f3a18c7942175cfba77
Hannakaroline <karolinepalacios@gmail.com>
2021-06-20 20:13:41 -0400

    Add some docs for the bookings_controller

--- reservas_api/app/controllers/bookings_controller.rb
+++ reservas_api/app/controllers/bookings_controller.rb
@@ -1,9 +1,12 @@
 class BookingsController < AuthenticatedController
+
+  # Lista as reservas feitas por um usuário no futuro que não foram canceladas
   def index
     @bookings = @current_user.bookings.where('bookings.cancelled = false AND bookings.from > ?', DateTime.now)
     render json: @bookings
   end
 
+  # Cancela uma reserva
   def cancel
     @booking = @current_user.bookings.find_by(id: params[:id])
     unless @booking.present?
